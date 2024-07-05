5339a525107422f4b3cdbdeb9cfb8f8d324167c9
Nazim <43852326+Nazim86@users.noreply.github.com>
2024-07-05 11:27:22 +0400

    Delete erp-aero directory

--- erp-aero/src/error-handler/error-handler.ts
+++
@@ -1,8 +0,0 @@
-export const errorMessage = (msg:string,field:string)=>{
-    return {
-        errorsMessages: [{
-            message: msg,
-            field: field
-        }]
-    }
-}
--- erp-aero/src/error-handler/result-type.ts
+++
@@ -1,7 +0,0 @@
-import {ResultCode} from "./result-code-enum";
-
-export type Result<T> = {
-    code: ResultCode
-    data: T,
-    message?: string
-}
